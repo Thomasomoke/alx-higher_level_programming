1d77ee3c9d6f08f63715bda0e20fbd26692de8aa
Thomas Omoke <omokethomas644@gmail.com>
2024-06-18 15:47:37 +0000

    h

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 class Rectangle {
     constructor(w, h) {
-        this.w = width;
-        this.h = height;
+        this.width = w;
+        this.height = h;
     }
 }
+
+let rect = new Rectangle(this.width, this.height);
 
