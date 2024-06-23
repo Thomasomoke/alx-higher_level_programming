afc94b5077f75b26784952140818e76bd9ef976f
Thomas Omoke <omokethomas644@gmail.com>
2024-06-22 22:40:39 -0700

    orm

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,27 +1,25 @@
-#!/usr/bin/python3
-import MySQLdb
-import sys
+ #!/usr/bin/python3
+ """
+0-select_states.py module
+
+This script connects to a MySQL database and retrieves all rows from
+the 'states' table. It takes three command-line arguments: the MySQL
+username, password, and database name.
+
+Usage:
+    ./0-select_states.py <mysql_username> <mysql_password> <database_name>
+"""
+ import MySQLdb
+ import sys
 
 if __name__ == "__main__":
-    try:
-        db = MySQLdb.connect(
-            host="localhost",
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3],
-            port=3306
-        )
+  db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
 
-        c = db.cursor()
-        c.execute("SELECT * FROM states")
-        rows = c.fetchall()
-        for row in rows:
-            print(row)
-    except MySQLdb.Error as e:
-        print(f"Error: {e}")
-    finally:
-        if c:
-            c.close()
-        if db:
-            db.close()
+  c = db.cursor()
+  c.execute("SELECT * FROM states")
+  rows = c.fetcha11()
+  for row in rows:
+    print(row)
+    c.close()
+    db.close()
 
