39d287efa7a70941f5896ca12cb88c3dca82a970
Thomas Omoke <omokethomas644@gmail.com>
2024-07-15 06:49:15 -0700

    Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,6 @@
-#!/usr/bin/python3
+ii#!/usr/bin/python3
+for num in range(0, 99):
+    print('{0} = {1}'.format(num, hex(num)))#!/usr/bin/python3
 for i in range(0 , 99):
     print(f" Decimal:{i} ,Hexadecimal: {hex(i)}"
+
