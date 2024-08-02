0d9433d0bd154fee3b96ae2e4f013c3b78ca341c
Thomasomoke <omokethomas644@gmail.com>
2024-08-02 03:37:32 -0700

    a script that reads and prints the content of a file.

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile("./t.txt", "utf-8", (error, data) => {
+fs.readFile("process.argv[2]", "utf8", function(error, data) => {
 	if (error) {
 		console.log(error)
 	} else {
